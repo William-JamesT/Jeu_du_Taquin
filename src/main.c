fab72b06d0d0f8fece82bcb7c5603ff528f2f349
gobe <theogobe7@gmail.com>
2025-11-15 12:53:05 -0500

    j'ai bz tt le projet faut que on regle le probleme d'image

--- src/main.c
+++ src/main.c
@@ -12,13 +12,13 @@ int main()
     c = CouleurParNom("red");
     ChoisirCouleurDessin(c);
     
-    ChargerImageFond("fondbleu.png");
+    ChargerImageFond("../fondbleu.png");
     EcrireTexte(430,50,"Bienvenue sur le jeu du Taquin !",2);
     /*Je remplis des triangle a l'endroit de mes boutons pour detecter si je clique dessus*/
     RemplirRectangle(450,280,150,89);
-    ChargerImage("bouton_jouer.png" ,450 ,280,0,0,150,89);
+    ChargerImage("../bouton_jouer.png" ,450 ,280,0,0,150,89);
     RemplirRectangle(450,375,150,89);
-    ChargerImage("bouton_quitter.png" ,450 ,375,0,0,150,89);
+    ChargerImage("../bouton_quitter.png" ,450 ,375,0,0,150,89);
     Bouton1P_or_Q();
 	while(Touche()!= XK_space);
 	FermerGraphique();
